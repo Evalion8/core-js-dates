cb91f22362071e623a3d1d1af3558ae6cee5dde3
Evalion8 <alisultanovaeva@gmail.com>
2025-03-24 15:47:20 +0300

    "feat: add 5 & 6 solutions"

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -85,8 +85,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -100,8 +100,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const timeDifference = end - start;
+  const dayDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+  return dayDifference + 1;
 }
 
 /**
